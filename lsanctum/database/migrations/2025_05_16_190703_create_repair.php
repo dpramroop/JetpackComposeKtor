1ac07e74da585e365e8d1c1c12bddd0f21fb8841
Darrin Ramroop <71991977+dpramroop@users.noreply.github.com>
2025-05-16 15:35:39 -0400

    database migrations

---
+++ lsanctum/database/migrations/2025_05_16_190703_create_repair.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('repair', function (Blueprint $table) {
+            $table->id();
+            $table->Integer("bin_id");
+            $table->Integer("rby_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('repair');
+    }
+};
